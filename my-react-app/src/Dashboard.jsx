fcc2cd83a2ede792d563381c03c881a2570b7b2d
Fayed Mauyag <faemauyag13@gmail.com>
2022-09-21 11:26:16 +0800

    Added Dashboard Page

--- my-react-app/src/Dashboard.jsx
+++ my-react-app/src/Dashboard.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Section from './components/Section'
+import SideMenu from './components/SideMenu'
 
 export default function Dashboard() {
     return (
-        <>
-        
-        </>
+        <div className="flex flex-row min-h-screen bg-gray-100 text-gray-800">
+            <SideMenu />
+            <Section />
+        </div>
     )
 }
